d3518ad1df7cc32976e1bd0ae5e772d416520bad
JOE TIMKO <jmt53@duke.edu>
2017-12-04 14:23:12 -0800

    SSL enabled

--- challenge/src/main/java/challenge/ChallengeApplication.java
+++ challenge/src/main/java/challenge/ChallengeApplication.java
@@ -1,10 +1,27 @@
 package challenge;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ChallengeApplication {
+
+
+//	@Bean
+//	public ServletWebServerFactory servletContainer() {
+//		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+//		return tomcat;
+//	}
+//
+//	private Connector createStandardConnector() {
+//		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//		connector.setPort(0);
+//		return connector;
+//	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeApplication.class, args);
