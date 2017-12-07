6cce31e8ee42e266dc003f489f9b27d31c162597
JOE TIMKO <jmt53@duke.edu>
2017-12-06 20:51:55 -0800

    Some clean up

--- challenge/src/main/java/challenge/CustomBasicAuthenticationEntryPoint.java
+++ challenge/src/main/java/challenge/CustomBasicAuthenticationEntryPoint.java
@@ -16,17 +16,16 @@ public class CustomBasicAuthenticationEntryPoint extends BasicAuthenticationEntr
     public void commence(final HttpServletRequest request,
                          final HttpServletResponse response,
                          final AuthenticationException authException) throws IOException, ServletException {
-        //Authentication failed, send error response.
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.addHeader("WWW-Authenticate", "Basic realm=" + getRealmName() + "");
 
         PrintWriter writer = response.getWriter();
-        writer.println("HTTP Status 401 : " + authException.getMessage());
+        writer.println("HTTP Status 401 : Unauthorized.  Bad Credentials");
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        setRealmName("MY_TEST_REALM");
+        setRealmName("MY_DEFAULT_REALM");
         super.afterPropertiesSet();
     }
 }
--- challenge/src/main/java/challenge/SecurityConfiguration.java
+++ challenge/src/main/java/challenge/SecurityConfiguration.java
@@ -1,12 +1,6 @@
 package challenge;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -14,38 +8,23 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.annotation.Resource;
-
-//import javax.naming.Context;
-//@WebAppConfiguration
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private static String REALM="MY_TEST_REALM";
+    private static String REALM="MY_DEFAULT_REALM";
 
-//    @Resource(name = "userDetailService")
-//    private UserDetailsService userDetailsService;
 
     @Autowired
     private UserDetailServiceImpl userDetailsService;
 
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication().withUser("batman").password("abc123").roles("ADMIN");
-//        auth.inMemoryAuthentication().withUser("tom").password("abc123").roles("USER");
-
-
-//        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
         auth.authenticationProvider(authenticationProvider());
-
-
     }
 
     @Override
@@ -87,8 +66,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    UserDetailsService createUserDetailsService(){
-//        return new UserDetailServiceImpl();
-//    }
 }
